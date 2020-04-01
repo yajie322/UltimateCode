c4fbfd8e6f6109b43b5ae977528f4e1e90e29399
rebecca <rebecca@rebeccade-MacBook-Pro-272.local>
2020-04-01 15:54:04 -0400

    delete hightlight.js

--- app/controllers/documents_controller.rb
+++ app/controllers/documents_controller.rb
@@ -7,6 +7,6 @@ class DocumentsController < ApplicationController
   def new
    #default: render index
     @question = "123"
-    @programming_languages = [['C','C'], ['C++','C++'], ['Ruby','Ruby']]
+    @programming_languages = [['C','C'], ['C++','C++'], ['Ruby','Ruby'], ['Python','Python']]
   end
 end
