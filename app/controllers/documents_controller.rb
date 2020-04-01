416574e06f3cbac1979d16510fa235e469466904
rebecca <rebecca@rebeccade-MacBook-Pro-272.local>
2020-04-01 19:44:44 -0400

    new func

--- app/controllers/documents_controller.rb
+++ app/controllers/documents_controller.rb
@@ -7,6 +7,8 @@ class DocumentsController < ApplicationController
   def new
    #default: render index
     @question = "123"
-    @programming_languages = [['C','C'], ['C++','C++'], ['Ruby','Ruby'], ['Python','Python']]
+    @programming_languages = [['Select Lang','c_cpp'], ['C','c_cpp'], ['C++','c_cpp'], ['CSS','css'], ['Go','golang'], ['HAML','haml'], ['HTML','html'], ['Java','java'], ['Javascript','javascript'], ['JSON','json'], ['MySql','mysql'], ['Python','python'], ['Ruby','ruby']]
+    @theme = [['Select Theme','ambiance'], ['Ambiance','ambiance'], ['Chaos','chaos'], ['Dracula','dracula'], ['Cobalt','cobalt'], ['Terminal','terminal'], ['Twilight','twilight'], ['Monokai','monokai'], ['Chrome','chrome'], ['Dawn','dawn'], ['Github','github'], ['Xcode','xcode'],]
+    @font_size = [['Font Size', 12], ['12', 12], ['14', 14], ['16', 16], ['18', 18], ['20', 20], ['40', 40], ['80', 80]]
   end
 end
