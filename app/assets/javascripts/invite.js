7011b9a6348e751d8b3008c89aa822e53590c96b
rebecca <rebecca@rebeccade-MacBook-Pro-272.local>
2020-04-01 15:17:23 -0400

    with hightlight and invite

--- app/assets/javascripts/invite.js
+++ app/assets/javascripts/invite.js
@@ -1,3 +1,27 @@
-function invite() {
-      alert('http://test.com')
-}
+window.onload = function () {
+      // your code 
+      var modal = document.getElementById("myModal");
+      var span = document.getElementsByClassName("close")[0];
+      document.getElementById("myTextarea").value = "for i in run";
+      
+      document.getElementById("myBtn").onclick = function () {
+            modal.style.display = "block";
+      }   
+      span.onclick = function () {
+            modal.style.display = "none";
+      }     
+      window.onclick = function (event) {
+            if (event.target == modal) {
+                  modal.style.display = "none";
+            }
+      }
+      
+      var editor = ace.edit("editor");
+      editor.setTheme("ace/theme/twilight");
+      editor.session.setMode("ace/mode/python");
+
+      
+};
+
+
+//hljs.initHighlightingOnLoad();
