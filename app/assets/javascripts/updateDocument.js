2cc6d0723cf1190c313f60bc9b1da3f48ee45aa4
William <wawawswswdwd@yahoo.com.tw>
2020-04-01 19:47:16 -0400

    OK

--- app/assets/javascripts/updateDocument.js
+++ app/assets/javascripts/updateDocument.js
@@ -4,15 +4,15 @@ $(document).ready(function(){
     editor.setTheme("ace/theme/twilight");
     editor.session.setMode("ace/mode/python");
     editor.session.on('change', function(){
-        //alert(editor.session.getValue())
+        alert(editor.session.getValue())
         $.ajax({
             url: "/documents/id",
             type: "PUT",
             data: {"content" : editor.session.getValue()},
             dataType: "text",
             success: function(data, requestStatus, xhrObject) {
-                //alert('successfully');
-                //alert(data);
+                alert('successfully');
+                alert(data);
             },
             error: function(xhr, ajaxOptions, thrownError) {
                 alert(xhr.status);
