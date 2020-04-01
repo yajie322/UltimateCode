6f4f40fc5b7c9f625dd9810237c446e62eb1d519
William <wawawswswdwd@yahoo.com.tw>
2020-04-01 19:31:11 -0400

    use ace package

--- app/assets/javascripts/updateDocument.js
+++ app/assets/javascripts/updateDocument.js
@@ -1,13 +1,18 @@
 $(document).ready(function(){
-    $('#MainContent').bind('input propertychange', function() {
+    //for editor
+    var editor = ace.edit("MainContent");
+    editor.setTheme("ace/theme/twilight");
+    editor.session.setMode("ace/mode/python");
+    editor.session.on('change', function(){
+        //alert(editor.session.getValue())
         $.ajax({
             url: "/documents/id",
             type: "PUT",
-            data: {"content" : $(this).text()},
+            data: {"content" : editor.session.getValue()},
             dataType: "text",
             success: function(data, requestStatus, xhrObject) {
-                alert('successfully');
-                alert(data);
+                //alert('successfully');
+                //alert(data);
             },
             error: function(xhr, ajaxOptions, thrownError) {
                 alert(xhr.status);
