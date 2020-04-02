cae37e77d68754f886aca156d37bc88ff0aa56d8
William <wawawswswdwd@yahoo.com.tw>
2020-04-02 15:31:44 -0400

    new document done; update content done

--- app/assets/javascripts/updateDocument.js
+++ app/assets/javascripts/updateDocument.js
@@ -1,10 +1,11 @@
 $(document).ready(function(){
     //for editor
     var editor = ace.edit("editor");
+    $('#docment_name').val($('#doc_name').val())
     editor.session.on('change', function(){
-        //alert(editor.session.getValue())
+        var url = "/documents/"+$('#doc_name').val();
         $.ajax({
-            url: "/documents/id",
+            url: url,
             type: "PUT",
             data: {"content" : editor.session.getValue()},
             dataType: "text",
@@ -19,3 +20,21 @@ $(document).ready(function(){
         });
       });
 });
+function updateDocName() {
+  alert('here!')
+  /*$.ajax({
+        url: url,
+        type: "PUT",
+        data: {"content" : editor.session.getValue()},
+        dataType: "text",
+        success: function(data, requestStatus, xhrObject) {
+            //alert('successfully');
+            //alert(data);
+        },
+        error: function(xhr, ajaxOptions, thrownError) {
+            alert(xhr.status);
+            alert(thrownError);
+        }
+    });
+  });*/
+}
--- app/controllers/documents_controller.rb
+++ app/controllers/documents_controller.rb
@@ -9,10 +9,24 @@ class DocumentsController < ApplicationController
     @programming_languages = [['Select Lang','c_cpp'], ['C','c_cpp'], ['C++','c_cpp'], ['CSS','css'], ['Go','golang'], ['HAML','haml'], ['HTML','html'], ['Java','java'], ['Javascript','javascript'], ['JSON','json'], ['MySql','mysql'], ['Python','python'], ['Ruby','ruby']]
     @theme = [['Select Theme','ambiance'], ['Ambiance','ambiance'], ['Chaos','chaos'], ['Dracula','dracula'], ['Cobalt','cobalt'], ['Terminal','terminal'], ['Twilight','twilight'], ['Monokai','monokai'], ['Chrome','chrome'], ['Dawn','dawn'], ['Github','github'], ['Xcode','xcode'],]
     @font_size = [['Font Size', 12], ['12', 12], ['14', 14], ['16', 16], ['18', 18], ['20', 20], ['40', 40], ['80', 80]]
+ 
+    count = Document.where(user_id: params[:user_id]).size
+    current_time = Time.now
+    document = Document.new
+    document.name = params[:user_id] + '_untitled_' + count.to_s
+    document.user_id = params[:user_id].to_i
+    document.content = ''
+    document.created_at = current_time
+    document.updated_at = current_time
+    document.save
+    @doc_name = params[:user_id] + '_untitled_' + count.to_s
   end
 
   def update
     #params['content'] to get content
-    @programming_languages = [['Select Lang','c_cpp'], [params['content'], params['content']]]
+    doc = Document.find_by(name: params[:id])
+    doc.update(content: params['content'])
+    doc.save!
+    @programming_languages = [doc.content, doc.content]
   end
 end
