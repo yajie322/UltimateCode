aaf9700aaf07aea8c2d9f0e5c53c2300075b5f06
William <wawawswswdwd@yahoo.com.tw>
2020-04-01 20:41:41 -0400

    comment alert

--- app/assets/javascripts/updateDocument.js
+++ app/assets/javascripts/updateDocument.js
@@ -2,15 +2,15 @@ $(document).ready(function(){
     //for editor
     var editor = ace.edit("editor");
     editor.session.on('change', function(){
-        alert(editor.session.getValue())
+        //alert(editor.session.getValue())
         $.ajax({
             url: "/documents/id",
             type: "PUT",
             data: {"content" : editor.session.getValue()},
             dataType: "text",
             success: function(data, requestStatus, xhrObject) {
-                alert('successfully');
-                alert(data);
+                //alert('successfully');
+                //alert(data);
             },
             error: function(xhr, ajaxOptions, thrownError) {
                 alert(xhr.status);
