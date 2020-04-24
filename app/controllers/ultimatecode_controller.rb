090f15e0d74cf55d49135226a22516eccc181e51
rebecca <rebecca@rebeccade-MacBook-Pro-272.local>
2020-04-23 22:19:51 -0400

    add css to selector

--- app/controllers/ultimatecode_controller.rb
+++ app/controllers/ultimatecode_controller.rb
@@ -1,10 +1,11 @@
 class UltimatecodeController < ApplicationController
   def index
    #default: render index
-    @document_list = [['Select File', '']]
+    @document_list = [[' Select File', '' ,:style => 'font-size:10px;']]
     document = Document.where(user_id: session[:user_id]).order(:name)
     document.each do |doc|
-        @document_list.append([doc.name, doc.id]) 
+        @document_list.append([doc.name, doc.id, :style => 'font-size:10px;']) 
     end
   end
 end
+
